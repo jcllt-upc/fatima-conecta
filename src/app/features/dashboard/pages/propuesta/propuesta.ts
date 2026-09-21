d3d9167ca8a3c9cdcd834922966772cb749fdafc
AQUIÑO <u202417479@upc.edu.pe>
2026-09-20 20:37:40 -0500

    feat: inicio modulo MP04-propuesta

---
+++ src/app/features/dashboard/pages/propuesta/propuesta.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-propuesta',
+  styleUrl: './propuesta.css',
+  templateUrl: './propuesta.html',
+})
+export class Propuesta {
+}
